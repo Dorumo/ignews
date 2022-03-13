1dc0d20e00f12aa2c1396e510ca0bda4bb7fdedc
Artur Moraes <artur.gomoraes@gmail.com>
2022-03-13 03:19:55 -0300

    componente e pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribrButton'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        hello
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Listen</span>
+          <h1>News about the <span>React</span>world.</h1>
+          <p>
+            Get access to all the publications. <br />
+            <span>for $9.90 montlhy</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
