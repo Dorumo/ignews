00965e794345ff5d9ae6c52fa05068a8315d9e5a
Artur Moraes <artur.gomoraes@gmail.com>
2022-03-14 00:50:35 -0300

    pagamentos no stripe

--- src/components/SubscribrButton/index.tsx
+++ src/components/SubscribrButton/index.tsx
@@ -1,16 +1,42 @@
 import styles from './styles.module.scss'
+import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-    return (
-      <button
-        type="button"
-        className={styles.subscribeButton}
-      >
-          Subscribe now
-      </button>  
-    )
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try { 
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs() 
+      
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (erro) {
+      alert(erro.message)
+    }
+
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  )
 }
